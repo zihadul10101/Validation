1376b374dacfbbe0faf4f1875df0c5bac1e51464
Zihadul Islam <zihadul10101@gmail.com>
2022-05-14 14:59:21 +0600

    ui completed

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,10 +1,64 @@
 import React from 'react';
-
+import { BsAt,BsFillTelephoneFill } from 'react-icons/bs';
+import { FaLock, FaUser } from 'react-icons/fa';
 const Register = () => {
     return (
-        <div>
-        <h1>register</h1> 
-        </div>
+        <>
+         <div className="register">
+                <div className="card">
+                <div className="image-logo">
+                        <img src="https://i.ibb.co/RQYHc7Q/reg.webp" />
+                    </div>
+                    <div className="auth">
+                        <h3>Register</h3>
+                        <form>
+                            {/* userName */}
+                            <div className="form-group">
+                                <label htmlFor="userName">User Name</label>
+                                <div className="icon-input">
+                                    <div className="icon"><FaUser /></div>
+                                    <input type="text" name='name' id="userName" placeholder="User Name" className="form-control" />
+
+                                </div>
+                            </div>
+                            {/* email */}
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <div className="icon-input">
+                                    <div className="icon"><BsAt /></div>
+                                    <input type="email" name='emai' id="email" placeholder="Emai" className="form-control" />
+
+                                </div>
+                            </div>
+                            {/* password */}
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <div className="icon-input">
+                                    <div className="icon"><FaLock /></div>
+                                    <input type="password" name='password' id="password" placeholder="Password" className="form-control" />
+                                </div>
+                            </div>
+                               {/* phone */}
+                               <div className="form-group">
+                                <label htmlFor="password">Phone</label>
+                                <div className="icon-input">
+                                    <div className="icon"><BsFillTelephoneFill /></div>
+                                    <input type="text" name='name' id="password" placeholder="your phone number" className="form-control" />
+                                </div>
+                            </div>
+                           
+                            <div className="form-group">
+                                <button className="btn btn-block">Register</button>
+                              
+
+                            </div>
+                           
+                        </form>
+                    </div>
+                   
+                </div>
+            </div> 
+        </>
     );
 };
 
